c303db9be50fa90e57f24029e130be33df689a83
raguperiyasamy <111695592+raguperiyasamy@users.noreply.github.com>
2023-01-17 04:07:33 -0500

    Add files via upload

---
+++ task-24/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import React from 'react';
+import Pricecard from './Components/Pricecard';
+function App() {
+  return <>
+    <Pricecard/>
+   </>
+}
+
+export default App;
